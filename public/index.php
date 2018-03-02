0e4a6918842cf42705c0b7388b9cbce0b5326178
Łukasz Pawłowski <lukasz.pawlowski.inf@gmail.com>
2018-03-02 08:26:00 +0100

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>SourceBox 1.2.1</title>
+</head>
+
+<body>
+<h1>SourceBox 1.2.1</h1>
+</body>
+</html>
